dc59641605637a1b383180bb101e881ed3917c64
Jessiah Yoder <j.yoder.dev@protonmail.com>
2018-01-08 09:54:39 -0700

    Added consecutive sixes feature.

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
-/*
-GAME RULES:
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he wishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-YOUR 3 CHALLENGES - Change the game to follow these rules:
-
-1. A player loses his ENTIRE score when he rolls two 6's in a row. After 
-that, it's the next player's turn. (Hint: Always save the previous dice 
-roll in a separate variable)
-
-2. Add an input field to the HTML where players can set the winning 
-score, so that they can change the predefined score of 100. (Hint: you 
-can read that value with the .value property in JavaScript. This is a 
-good opportunity to use Google to figure this out :)
-
-3. Add another dice to the game, so that there are two dices now. The 
-player looses his current score when one of them is a 1. (Hint: you will 
-need CSS to position the second dice, so take a look at the CSS code for 
-the first one.)
-*/
 var scores, roundScore, activePlayer, gamePlaying;
 
 init();
@@ -35,13 +11,20 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 		diceDOM.style.display = 'block';
 		diceDOM.src = 'dice-' + dice + '.png';
 		//3. Update the round score IF the rolled number was not a one.
-		if (dice > 1) {
+		if (dice != 1 && dice != 6) {
 			//Add score
 			roundScore += dice;
 			document.querySelector('#current-' + activePlayer).textContent = roundScore;
-		} else {
+			sixFlag = false;
+		} else if (dice = 1) {
 			//Change to next player
 			nextPlayer();
+		} else if (dice = 6) {
+			if (sixFlag = true) {
+				nextPlayer();
+			} else {
+				sixFlag = true;
+			}
 		}
 	}
 });
